d629d4909f9fb662ed184385d6e6b86b9c01c337
Daoud <99m.daoud@gmail.com>
2021-01-07 17:24:59 +0100

    aggiunto header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,66 @@
 <header>
-    
+    <div class="top-header">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#" class="uc">
+                        DC Power&#x2120; Visa<sup>&reg;</sup>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="uc">
+                        Additional DC Sites <span>&#x25BC;</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="bottom-header">
+        <div class="container">
+            <div class="nav-container">
+                <a href="{{ route('Home') }}" class="logo">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="DC Comics Logo">
+                </a>
+
+                <ul>
+                    <li>
+                        <a class="uc" href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a class="uc active" href="{{ route('Home') }}">Comics</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">TV</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">Games</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a class="uc" href="#">News</a>
+                    </li>
+                    <li>
+                        <a class="uc dropdown" href="#">Shop</a>
+                    </li>
+                </ul>
+
+                <div class="searchbar">
+                    <input type="text" placeholder="Search">
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
